b81241ca992bb9766e392077262c2ca657b47fab
irendon1993 <isaacrendon7@gmail.com>
2022-04-13 12:50:11 -0400

    Add comments to articles with controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  #This creates comments as a nested resource within articles. 
+  #This is another part of capturing the hierarchical relationship 
+  #that exists between articles and comments.
+  resources :articles do 
+    resources :comments
+  end
 end
